c2a222c958a57eeb5d1db1e090e38d4f30cd34fc
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2025-04-22 18:51:37 -0400

    Imagens

---
+++ atividade_abril/model/Articles.php
@@ -0,0 +1,25 @@
+<?php 
+    $articles = [
+        ["Como Aprender PHP", "Neste artigo, exploramos os primeiros passos para aprender PHP de forma eficiente.", "..\assets\images\artigo1.png"],
+        ["Boas Práticas de Programação", "Entenda como escrever códigos limpos e fáceis de manter.", "..\assets\images\artigo2.jpg"],
+        ["Desenvolvimento Web com PHP", "Uma introdução ao desenvolvimento de aplicações web utilizando PHP.", "..\assets\images\artigo3.png"],
+        ["O Que Há de Novo no PHP 8", "Veja as principais novidades da versão mais recente do PHP.", "..\assets\images\artigo4.png"],
+        ["Segurança em Aplicações PHP", "Dicas importantes para proteger sua aplicação contra ataques.", "..\assets\images\artigo5.png"],
+        ["PHP e MySQL", "Como integrar PHP com bancos de dados MySQL para construir aplicações dinâmicas.", "..\assets\images\artigo6.png"],
+        ["Laravel vs CodeIgniter", "Comparação entre dois dos frameworks PHP mais populares.", "..\assets\images\artigo7.jpg"],
+        ["Criando APIs com PHP", "Aprenda a criar APIs RESTful utilizando PHP puro.", "..\assets\images\artigo8.jpg"],
+        ["Trabalhando com Arrays em PHP", "Entenda como manipular arrays de forma eficiente.", "..\assets\images\artigo9.jpg"],
+        ["Introdução ao Composer", "Gerenciando dependências em projetos PHP com Composer.", "..\assets\images\artigo10.jpg"],
+        ["PHP Orientado a Objetos", "Conceitos básicos e avançados da programação orientada a objetos em PHP.", "..\assets\images\artigo11.png"],
+        ["Trabalhando com Sessões", "Gerencie sessões de usuário de forma segura e eficaz.", "..\assets\images\artigo12.jpg"],
+        ["Gerando PDFs com PHP", "Crie documentos PDF dinamicamente usando bibliotecas PHP.", "..\assets\images\artigo13.jpg"],
+        ["Formulários e Validações", "Como criar formulários seguros com validação de dados no PHP.", "..\assets\images\artigo14.jpg"],
+        ["Criando um Blog com PHP", "Um passo a passo para criar um blog simples com PHP e MySQL.", "..\assets\images\artigo15.jpg"],
+        ["Enviando Emails com PHP", "Como configurar o envio de emails em suas aplicações.", "..\assets\images\artigo16.jpg"],
+        ["Upload de Arquivos", "Saiba como permitir uploads de arquivos com segurança.", "..\assets\images\artigo17.png"],
+        ["Manipulando Arquivos no Servidor", "Leitura e escrita de arquivos utilizando funções nativas do PHP.", "..\assets\images\artigo18.jpg"],
+        ["PHP e JSON", "Trabalhando com dados em formato JSON em aplicações PHP.", "..\assets\images\artigo19.png"],
+        ["Cache com PHP", "Técnicas de cache para melhorar a performance de seus sites.", "..\assets\images\artigo20.jpg"]
+    ];
+?>
+    
---
+++ atividade_abril/model/Musics.php
@@ -0,0 +1,20 @@
+<?php 
+    $musics = [
+        ["Jean Michel Jarre", "Oxygene Pt.2", "..\assets\images\oxygene.jpg", "Classic"],
+        ["Eurythmics", "When Tomorrow Comes", "..\assets\images\when_tomorrow_comes.jpg", "Pop"],
+        ["MaxAmillion", "Fat Boy", "..\assets\images\maxamillion.jpg", "Garage"],
+        ["Iron Maiden", "2 Minutes to Midnight", "..\assets\images\iron_2minutes.jpg", "Heavy Metal"],
+        ["Blinding Lights", "The Weeknd", "..\assets\images\Blinding.jpg", "Pop"],
+        ["Bohemian Rhapsody", "Queen", "..\assets\images\bohemian.webp", "Rock"],
+        ["Shape of You", "Ed Sheeran", "..\assets\images\shape_of_you.jpg", "Pop"],
+        ["Smells Like Teen Spirit", "Nirvana", "..\assets\images\smells_like_teen_spirit.jpg", "Grunge"],
+        ["Billie Jean", "Michael Jackson", "..\assets\images\billie_jean.jpg", "Pop"],
+        ["Lose Yourself", "Eminem", "..\assets\images\lose_yourself.jpg", "Rap"],
+        ["Rolling in the Deep", "Adele", "..\assets\images\olling_in_the_deep.jpg", "Soul"],
+        ["Hotel California", "Eagles", "..\assets\images\hotel_california.jpg", "Rock"],
+        ["Bad Guy", "Billie Eilish", "..\assets\images\bad_guy.jpg", "Alternative"],
+        ["Thinking Out Loud", "Ed Sheeran", "..\assets\images\hinking_out_loud.png", "Pop"],
+        ["Stand By Me", "Ben E King", "..\assets\images\stand_by_me.jpg", "Blues"],
+        ["Porto Solidão", "Jessé", "..\assets\images\porto_solidao.jpg", "MPB"]
+    ];
+?>
